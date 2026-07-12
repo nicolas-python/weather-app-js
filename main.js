bb673914944d0198d5ca4c344327aa3391b64d29
Nicolas <nicolasmader001@gmail.com>
2026-07-12 16:03:09 +0200

    feat(main): read city input value

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const search_button = document.getElementById("search_button");
-
+const city_name = document.getElementById("city_name");
 
 
 search_button.addEventListener("click", function()
 {
-    console.log("Suche");
+    document.getElementById("city_name");
+    console.log(city_name.value);
+
 });
