06edcbd42e75aefb236d72ae4445bc28cc2d0e91
Nicolas <nicolasmader001@gmail.com>
2026-07-13 13:11:10 +0200

    feat(api) connect weather api request

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const search_button = document.getElementById("search_button");
 const city_name = document.getElementById("city_name");
-
+const API_KEY = "165dd84d9346892c4d057d6d1265ad33";
 
 search_button.addEventListener("click", function()
 {
     document.getElementById("city_name");
+    const city = city_name.value;
+
     console.log(city_name.value);
 
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        });
 });
+
